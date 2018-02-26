8ddbd25dc849c4ed5ba4d7b1ea492bd623128f46
Nuermuhaimumaiti Abuli <nabuli@VENTERA.com>
2018-02-26 11:56:12 -0500

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs=require('fs');
+const port=process.env.PORT || 3000;
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
@@ -22,14 +23,13 @@ app.use((req,res,next) => {
 // app.use((req,res,next)=>{
 //    res.render('maintenance.hbs');
 // });
-
 //app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear',()=>{
   return new Date().getFullYear()
 });
 hbs.registerHelper('screamIt',(text)=>{
-  return text.toUpperCase();
+  return text.toUpperCase();s
 })
 
 // setup handler for the HTTP request
@@ -53,6 +53,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
